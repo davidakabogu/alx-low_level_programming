5d831648d6ff504d6f8d35454bad1337dd6cac0d
King David <akabogudavid@gmail.com>
2023-02-16 04:21:15 -0800

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints and array of characters.
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
