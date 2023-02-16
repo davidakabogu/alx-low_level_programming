10ef95800fa7cf9bc991214ea6b610ac3c95d57a
King David <akabogudavid@gmail.com>
2023-02-16 04:41:55 -0800

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints an array of characters.
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
