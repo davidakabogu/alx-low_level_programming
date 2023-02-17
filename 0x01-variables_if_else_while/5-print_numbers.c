5b09439a3676ddf82c5922b4abaaa7390224d9fa
King David <akabogudavid@gmail.com>
2023-02-17 06:20:48 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints numbers 1-9
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+
+	return (0);
+}
