c6aff4b0a26095a62461e24b223a61abcd79d7bd
King David <akabogudavid@gmail.com>
2023-02-21 09:56:07 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @num1: parameter to be passed
+ *
+ * @num2: parameter to be passed
+ *
+ * Return: Always 0
+ */
+
+int add(int num1, int num2)
+{
+	return ((num1 + num2));
+}
