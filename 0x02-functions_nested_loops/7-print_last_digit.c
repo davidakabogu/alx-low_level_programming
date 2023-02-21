5c14342b172db8207c55c30a3f4b51617c6df7b8
King David <akabogudavid@gmail.com>
2023-02-21 07:26:43 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @k: parameter to be passed
+ *
+ * Return: Always 0
+ */
+
+int print_last_digit(int k)
+{
+	int m;
+
+	m = k % 10;
+
+	if (k < 0)
+	{
+		m = -(m);
+	}
+
+	return (m);
+}
