8fb007f614d0fa36db449fc686ee53523e30f42c
King David <akabogudavid@gmail.com>
2023-02-21 05:54:50 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ *
+ * @c: parameter to be printed
+ *
+ * Return: 1 if c is lower c and 0 otherwise
+ *
+ */
+
+int _islower(int c)
+{
+	c = 0;
+
+	c++;
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
