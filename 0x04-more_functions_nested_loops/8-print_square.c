c6a81f91e794c2c4c7bbe21eb3cf4f05f33791d6
King David <akabogudavid@gmail.com>
2023-02-23 10:19:13 -0800

    sunshine sqaure inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - This function prints a square 
+ *
+ * @size: parameter to be passed
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
