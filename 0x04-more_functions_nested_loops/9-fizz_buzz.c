38c55746d7c061990f297e6b635d68748286c756
King David <akabogudavid@gmail.com>
2023-02-23 14:28:55 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,29 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 != 0) && (i % 5 != 0))
+		if ((i % 3 != 0) && (i % 5 == 0))
 		{
-			printf("%d ", i);
+			printf(" Buzz");
 		}
 
 		else if ((i % 3 == 0) && (i % 5 != 0))
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
+		}
+
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}
 
 		else
 		{
-			printf("Buzz ");
+			printf(" %d", i);
 		}
 	}
 
