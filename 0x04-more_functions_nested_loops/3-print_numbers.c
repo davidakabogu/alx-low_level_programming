47a53af007420951c7c65f8d2f0455d79b065fde
King David <akabogudavid@gmail.com>
2023-02-23 05:25:45 -0800

    Numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - This function prints numbers 0 - 9
+ *			followed by a new line.
+ */
+
+void print_numbers(void)
+{
+	int c = 48;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
