304e721acbfe5913d28c83e025bfe02e254d1bb7
King David <akabogudavid@gmail.com>
2023-02-23 11:25:28 -0800

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints Fizz Buzz
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 != 0) && (i % 5 != 0))
+		{
+			printf("%d ", i);
+		}
+
+		else if ((i % 3 == 0) && (i % 5 != 0))
+		{
+			printf("Fizz ");
+		}
+
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+
+		else
+		{
+			printf("Buzz ");
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
