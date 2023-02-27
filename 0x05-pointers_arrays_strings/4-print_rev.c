1f1483fc83681b313ab7875032fd03dbe2f02e6a
King David <akabogudavid@gmail.com>
2023-02-27 04:52:47 -0800

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - This function prints a string in reverse
+ *		followed by a new line.
+ *
+ * @s: parameter to be passed.
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+	
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = (i - 1); j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
