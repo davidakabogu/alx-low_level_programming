4fb65c55264cc7c17b72fcdf68c40cab75fcd499
King David <akabogudavid@gmail.com>
2023-03-10 09:33:34 -0800

    irony sarcasm argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this program multiplies two numbers
+ *
+ * @argc: stores number of command line arguments passed by the user
+ * @argv: array of character pointers listing all the arguments
+ *
+ * Return: returns 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+
+	return (0);
+}
