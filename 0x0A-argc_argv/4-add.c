5caa2f1d18f5c63105d32624793dbc2252474bb6
King David <akabogudavid@gmail.com>
2023-03-10 12:17:06 -0800

    infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,14 +14,8 @@
 int main(int argc, char *argv[])
 {
 	int i, j, result = 0;
-	
-	if (argc <= 1)
-	{
-		printf("0\n");
-		return (0);
-	}
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
