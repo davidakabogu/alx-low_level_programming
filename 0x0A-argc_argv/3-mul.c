af4a50a0c9f276aace1cc4dc5dfdacd39b0c21bc
King David <akabogudavid@gmail.com>
2023-03-10 11:03:33 -0800

    to infinity and beyond

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
  * Return: returns 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this program adds positive numbers
+ *
+ * @argc: stores number of command line arguments passed by the user
+ * @argv: array of character pointers listing all the arguments
+ *
+ * Return: returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, result = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+
+				return (1);
+			}
+		}
+
+		result += atoi(argv[i]);
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
