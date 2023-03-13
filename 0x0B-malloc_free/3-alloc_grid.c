92022eea17ff69f550fc28a2d947b1444b976e3c
King David <akabogudavid@gmail.com>
2023-03-13 08:07:33 -0700

    better wake up

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,6 +32,10 @@ int **alloc_grid(int width, int height)
 		ptr[i] = (int *) malloc(sizeof(int) * width);
 		if (ptr[i] == 0)
 		{
+			for (i--; i >= 0; i--)
+			{
+				free(ptr[i]);
+			}
 			free(ptr);
 			return (0);
 		}
