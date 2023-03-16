757e366a81c1eaa0f17c266b5a53a502bafa6e97
King David <akabogudavid@gmail.com>
2023-03-16 11:49:52 -0700

    finite dissapointment infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,37 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <ctype.h>
 
 /**
- * is_zero - checks if a number is zero
- * @argv: ARGument Vector.
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
+ *
  * Return: no return.
  */
-
-void is_zero(char *argv[])
+void _is_zero(char *argv[])
 {
-	int i, n1 = 1, n2 = 1;
+	int i, isn1 = 1, isn2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
-	{
 		if (argv[1][i] != '0')
 		{
-			n1 = 0;
+			isn1 = 0;
 			break;
 		}
-	}
 
 	for (i = 0; argv[2][i]; i++)
-	{
 		if (argv[2][i] != '0')
 		{
-			n2 = 0;
+			isn2 = 0;
 			break;
 		}
-	}
 
-	if (n1 == 1 || n2 == 1)
+	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
 		exit(0);
@@ -39,54 +35,52 @@ void is_zero(char *argv[])
 }
 
 /**
- * initialize_array - set memery to zero in a new array
- * @ar: chararacter array.
- * @lar: length of the character array.
- * Return: pointer of a character array.
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
  */
-
-char *initialize_array(char *ar, int lar)
+char *_initialize_array(char *ar, int lar)
 {
-	int i;
+	int i = 0;
 
 	for (i = 0; i < lar; i++)
-	{
 		ar[i] = '0';
-	}
-
 	ar[lar] = '\0';
 	return (ar);
 }
 
 /**
- * checknum - checks length of the number and checks if number is in base 10.
- * @argv: ARGument Vector.
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
  * @n: row of the array.
+ *
  * Return: length of the number.
  */
-
-int checknum(char *argv[], int n)
+int _checknum(char *argv[], int n)
 {
 	int ln;
 
 	for (ln = 0; argv[n][ln]; ln++)
-	{
 		if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
-	}
+
 	return (ln);
 }
 
 /**
- * main - this is a program that multiplies two positive numbers.
- * @argc: ARGument Count
- * @argv: ARGument Vector.
- * Return: returns 0
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
 	int ln1, ln2, lnout, add, addl, i, j, k, ca;
@@ -94,11 +88,11 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	ln1 = checknum(argv, 1), ln2 = checknum(argv, 2);
-	is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
-	if (nout == 0)
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
 		printf("Error\n"), exit(98);
-	nout = initialize_array(nout, lnout);
+	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
@@ -118,7 +112,7 @@ int main(int argc, char *argv[])
 			if (nout[0] != '0')
 				break;
 			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = initialize_array(nout, lnout);
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
 			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
