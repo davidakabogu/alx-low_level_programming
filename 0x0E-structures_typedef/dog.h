a802ee07253459cf3d3264707805fb0b602239d4
King David <akabogudavid@gmail.com>
2023-03-20 05:14:27 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - this structures holds the variables for
+ *		describing a dog.
+ * @name: this is the name of the dog.
+ * @age: this is the age of the dog.
+ * @owner: this is the name of the owner of the dog.
+ */
 struct dog
 {
 	char *name;
