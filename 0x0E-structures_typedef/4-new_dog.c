d4a9e7c37420b5edde64c0b31364ba4ca19b397c
King David <akabogudavid@gmail.com>
2023-03-20 07:59:19 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,34 +19,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (iowner = 0; owner[iowner] != '\0'; iowner++)
 		;
 
-	ptr = malloc(sizeof(ptr));
+	ptr = malloc(sizeof(*ptr));
 	if (ptr == 0 || !(name) || !(owner))
 	{
 		free(ptr);
-		return (ptr);
+		return (0);
 	}
 
 	ptr->name = malloc(iname + 1);
 	ptr->owner = malloc(iowner + 1);
-
 	if (!(ptr->name) || !(ptr->owner))
 	{
 		free(ptr->name);
 		free(ptr->owner);
 		free(ptr);
+		return (0);
 	}
-
 	for (i = 0; i < iname; i++)
 	{
 		ptr->name[i] = name[i];
 	}
 	ptr->name[i] = '\0';
-
 	for (i = 0; i < iowner; i++)
 	{
 		ptr->owner[i] = owner[i];
 	}
 	ptr->owner[i] = '\0';
+
 	ptr->age = age;
 	return (ptr);
 }
