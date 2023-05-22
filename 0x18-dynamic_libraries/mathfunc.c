7784a42e72bae3776f4695341b647e72283a0983
David King <akabogudavid@gmail.com>
2023-05-22 11:19:58 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/mathfunc.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * add - this function sums two integers
+ * @a: int a
+ * @b: int b
+ * Return: returns the sum of both integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - this function subtracts two integers
+ * @a: int a
+ * @b: int b
+ * Return: returns the difference of both integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - this function multiplies two integers
+ * @a: int a
+ * @b: int b
+ * Return: returns the product of both integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - this function divides two integers
+ * @a: int a
+ * @b: int b
+ * Return: returns the division of both integers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - this function divides and returns the remainder of two integers
+ * @a: int a
+ * @b: int b
+ * Return: returns the remainder of both integers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
