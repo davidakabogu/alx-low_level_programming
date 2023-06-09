29f3fd7b99c9679d713f84fe32118a1ed6f92bb9
David King <akabogudavid@gmail.com>
2023-06-09 12:30:40 -0700

    C hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,16 +12,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 
-	hash_table = malloc(sizeof(hash_table_t));
+	hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
 
 	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_table_t) * size);
+	hash_table->array = (hash_table_t **)malloc(sizeof(hash_table_t *) * size);
 	if (hash_table->array == NULL)
 	{
+		free(hash_table);
 		return (NULL);
 	}
 
