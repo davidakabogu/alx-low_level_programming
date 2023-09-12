7a714182a8ef85f734cbada87a693dd1840eab95
DavidAkabogu <Akabogudavid@gmail.com>
2023-09-12 04:49:01 +0100

    search algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return (-1);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("Value checked array[%u] = [%d]\n", i, array[i]);
+        if (value == array[i])
+            return (i);
+    }
+    return (-1);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9};
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    return (EXIT_SUCCESS);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+/* function prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
